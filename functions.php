6c313211bfc22c77c20c40654b4c4182fdcf33be
Evan Turner <evturn@gmail.com>
2015-03-03 06:11:01 -0500

    commented out normalize

--- functions.php
+++ functions.php
@@ -2,7 +2,7 @@
 
 function wpmi_theme_styles() {
 
-	wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/assets/css/normalize.css');
+	// wp_enqueue_style( 'normalize_css', get_template_directory_uri() . '/assets/css/normalize.css');
 	wp_enqueue_style( 'main_css', get_template_directory_uri() . '/style.css');
 
 }
