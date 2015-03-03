c3b23436dc955d8f379c10726d9a2f4beff7d22a
Evan Turner <evturn@gmail.com>
2015-03-03 03:45:41 -0500

    header and footer add to php files

---
+++ footer.php
@@ -0,0 +1,12 @@
+    <footer class="site-footer">
+        <div class="pagelist">
+        <a href="Contact.html">Contact</a> /
+        <a href="#">Policy</a> /
+        <a href="#">Terms</a> 
+        </div>
+        <div class="inner">
+             <section class="copyright">All content copyright <a href="/">Music Insider</a> &copy; 2015 &bull; All rights reserved.</section>
+                  <img src=''/>
+
+        </div>
+    </footer>
---
+++ header.php
@@ -0,0 +1,13 @@
+<header id="site-head" style="background-image: url('assets/images/headbg.jpg')">
+    <div class="vertical">
+        <div id="site-head-content" class="inner">
+            
+
+            <h1 class="blog-title">Minimal Theme</h1>
+            <h2 class="blog-description">Sup br0z
+</h2>
+        </div>
+        
+    </div>
+</header>
+<br/>
