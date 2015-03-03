6e456b2051828963f77ac6f69e15d83f02c9c3e6
Evan Turner <evturn@gmail.com>
2015-03-03 03:42:14 -0500

    main script renamed from index to app

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ add_action('wp_enqueue_scripts', 'wpmi_theme_styles');
 
 function wpmi_theme_js() {
 
-	wp_enqueue_script('index_js', get_template_directory_uri() . '/assets/js/index.js', array('jquery'), '', true);
+	wp_enqueue_script('app_js', get_template_directory_uri() . '/assets/js/app.js', array('jquery'), '', true);
 	wp_enqueue_script('jquery_fitvids_js', get_template_directory_uri() . '/assets/js/jquery.fitvids.js', array('jquery'), '', true);
 	wp_enqueue_script('pace_js', get_template_directory_uri() . '/assets/js/pace.js', array('jquery'), '', true);
 }
